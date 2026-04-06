fa25f45dfc58de199b494d177cbda49184e78705
DevBrigid <mukamibrigid20@gmail.com>
2026-04-06 19:59:34 +0300

    Complete compulsory lab

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-
+require('datejs')
+function combineUsers(...args){
+  const combinedObject = {
+    users : []
+  };
+  for(const arr of args){
+    combinedObject.users = [...combinedObject.users,...arr];
+  };
+  combinedObject.merge_date = new Date().toString("M/d/yyyy")
+  return combinedObject;
+}
 
 
 module.exports = {
